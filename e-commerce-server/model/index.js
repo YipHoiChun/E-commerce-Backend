d1c22639b9b40da808867fda99a070d0393536f0
YipHoiChun <yiphoichun29@gmail.com>
2021-04-30 14:38:13 +0800

    delete not use model

--- e-commerce-server/model/index.js
+++ e-commerce-server/model/index.js
@@ -3,25 +3,6 @@ const Schema = mongoose.Schema;
 const model = mongoose.model.bind(mongoose);
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
-// const productSchema = Schema({
-//   id: ObjectId,
-//   name: String,
-//   image: String,
-//   price: Number,
-//   description: String,
-//   manufacturer: { type: ObjectId, ref: 'Manufacturer' }
-// });
-
-// const manufacturerSchema = Schema({
-//   id: ObjectId,
-//   name: String,
-// });
-
-
-// const Product = model('Product', productSchema);
-// const Manufacturer = model('Manufacturer', manufacturerSchema);
-
-// module.exports = { Product, Manufacturer};
 const commoditySchema = Schema({
   id: ObjectId,
   name: String,
